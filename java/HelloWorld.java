3de8e91764da889b169301af26a4e6bcecb8fe5a
Sainyam Kapoor <sainyamkapoor@yahoo.com>
2014-01-06 18:14:03 +0530

    Added the Java directory as well as first JAVA program

---
+++ java/HelloWorld.java
@@ -0,0 +1,17 @@
+/*Simple hello world program
+Version 1.00 06-01-2014
+By Sainyam Kapoor
+*/
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		String[] hello=new String[3];
+		hello[0]="Hello ,World";
+		hello[1]="I am Sainyam Kapoor";
+		hello[2]="This will be my first Java program";
+		for( String temp : hello)
+			System.out.println(temp);
+	}
+}
