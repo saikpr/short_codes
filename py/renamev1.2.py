6a0c13f830afb8f7ec5fc1d58d7edf2301574479
Sainyam Kapoor <sainyamkapoor@yahoo.com>
2014-02-17 11:19:10 -0800

    Added data to renamev1.2 .py

--- py/renamev1.2.py
+++ py/renamev1.2.py
@@ -1,8 +1,7 @@
 import os
-i=0
 replace = ['Watch','Online for Free - Viooz','- YouTube','.HDTV.XviD-2HD'  ,'.HDTV.XviD-SYS','.HDTV.XviD-XII' ,'.HDTV.XviD-aAF','.x264-BoB'
 ,'Dvdrip-Topaz' ,' Free and reliable flash video hosting',' NovaMov' ,' online - -','YouTube',' HDTV XviD LOL',
-'HDTV','x264', 'ASAP','LOL']
+'HDTV','x264', 'ASAP','LOL','Online']
 
 for filename in os.listdir("."):
     filename1=filename
