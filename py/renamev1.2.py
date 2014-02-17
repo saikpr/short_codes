3cf321504bf1d56763218bc9f76c2e93ad627b42
Sainyam Kapoor <sainyamkapoor@yahoo.com>
2014-02-17 00:27:28 -0800

    Added another version of Rename which removes particular text from the file

---
+++ py/renamev1.2.py
@@ -0,0 +1,12 @@
+import os
+i=0
+replace = ['Watch','Online for Free - Viooz','- YouTube','.HDTV.XviD-2HD'  ,'.HDTV.XviD-SYS','.HDTV.XviD-XII' ,'.HDTV.XviD-aAF','.x264-BoB'
+,'Dvdrip-Topaz' ,' Free and reliable flash video hosting',' NovaMov' ,' online - -','YouTube',' HDTV XviD LOL',
+'HDTV','x264', 'ASAP','LOL']
+
+for filename in os.listdir("."):
+    filename1=filename
+    for content in replace:
+        filename1 = filename1.replace(content,"")
+
+    os.rename(filename, filename1)
