d6eba3341b6105d63f4a7100d947b3c0845cd3b6
Sainyam Kapoor <sainyamkapoor@yahoo.com>
2014-09-15 01:55:14 +0530

    Edited Matrix multiplication to include time vs thread count

--- c/mat_mul_pthreads.c
+++ c/mat_mul_pthreads.c
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
+#include <stdlib.h>
 //int a[100][100]={{1,2,3},{4,5,6},{7,8,9}},b[100][100]={{1,2,3},{4,5,6},{7,8,9}},c[100][100],n;
 typedef struct coor {int x_st,y_st,x_ed,y_ed;}  xycood;
 int a[100][100],b[100][100],c[100][100],n;
@@ -18,14 +20,19 @@ int main()
 {
 	pthread_t tn[10000];  // max supported threads 10000
 	xycood xyvar[10000];
-	int threadcount,i,j,it[10000];
+	int threadcount,threadmaxcount,i,j,it[10000],til;
 	float gpvar;
-	printf("Enter the Number of threads :");
-	scanf("%d",&threadcount);
+	clock_t begin, end;
+	double time_spent;
+
+
+	printf("Enter the Max Number of threads :");
+	scanf("%d",&threadmaxcount);
 	printf("\nEnter the Size N:");
 	scanf("%d",&n);
-	gpvar=(n*n)/(float)threadcount;
+	
 	printf("\nEnter the First Matrix");
+	
 	for (i=0;i<n;i++)
 		for (j=0;j<n;j++)
 		{
@@ -40,39 +47,57 @@ int main()
 		}
 	
 	
-
-	xyvar[i].x_st=0;
-	xyvar[i].y_st=0;
-	xyvar[i].x_ed=(gpvar- 1)/n;
-	xyvar[i].y_ed=(((int)gpvar-1)%n);
-	//printf("%f \n",gpvar);
-	for(i=1;i<threadcount;i++)
-		{ 
-			xyvar[i].x_st=xyvar[i-1].x_ed + (xyvar[i-1].y_ed+1)/n -(xyvar[i-1].y_ed)/n;
-			
-			xyvar[i].y_st=(xyvar[i-1].y_ed+1)%n;
-			
-			xyvar[i].y_ed= (int)((i+1)*gpvar-1)%n;
-
-			xyvar[i].x_ed= (int)((i+1)*gpvar-1)/n;
-		}
-	/*
-	for(i=0;i<threadcount;i++)
-		{ 
-
-			printf("%d %d %d %d %d\n",xyvar[i].x_st,xyvar[i].y_st,xyvar[i].x_ed,xyvar[i].y_ed,((xyvar[i].x_ed-xyvar[i].x_st )*n+(xyvar[i].y_ed-xyvar[i].y_st)+1));
+	/* //Random Numbers
+	for (i=0;i<n;i++)
+		for (j=0;j<n;j++)
+		{
+			a[i][j]=rand() % 20000;
+			b[i][j]=rand() % 20000;
+			c[i][j]=0;
 		}
 	*/
-	for (i=0;i<threadcount;i++)
-		{
-			it[i]= pthread_create(&tn[i],NULL,multimat,(void *)(xyvar+i));
-		}
-	for (i=0;i<threadcount;i++)
-		{
-			pthread_join(tn[i],NULL);
-		}
+	
+	for (threadcount=1;threadcount<=threadmaxcount;threadcount++)
+	{	gpvar=(n*n)/(float)threadcount;
+		xyvar[i].x_st=0;
+		xyvar[i].y_st=0;
+		xyvar[i].x_ed=(gpvar- 1)/n;
+		xyvar[i].y_ed=(((int)gpvar-1)%n);
+		//printf("%f \n",gpvar);
+		for(i=1;i<threadcount;i++)
+			{ 
+				xyvar[i].x_st=xyvar[i-1].x_ed + (xyvar[i-1].y_ed+1)/n -(xyvar[i-1].y_ed)/n;
+				
+				xyvar[i].y_st=(xyvar[i-1].y_ed+1)%n;
+				
+				xyvar[i].y_ed= (int)((i+1)*gpvar-1)%n;
 
-	for (i=0;i<n;i++)
+				xyvar[i].x_ed= (int)((i+1)*gpvar-1)/n;
+			}
+		/*
+		for(i=0;i<threadcount;i++)
+			{ 
+
+				printf("%d %d %d %d %d\n",xyvar[i].x_st,xyvar[i].y_st,xyvar[i].x_ed,xyvar[i].y_ed,((xyvar[i].x_ed-xyvar[i].x_st )*n+(xyvar[i].y_ed-xyvar[i].y_st)+1));
+			}
+		*/
+		begin = clock();
+		for (i=0;i<threadcount;i++)
+			{
+				it[i]= pthread_create(&tn[i],NULL,multimat,(void *)(xyvar+i));
+			}
+		for (i=0;i<threadcount;i++)
+			{
+				pthread_join(tn[i],NULL);
+			}
+		end = clock();
+		time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		printf("Elapsed: %f second with %d Thread count\n", time_spent,threadcount);
+
+
+
+	}
+	/*for (i=0;i<n;i++)
 		{	
 			for (j=0;j<n;j++)
 			{
@@ -80,6 +105,7 @@ int main()
 			}
 			printf("\n");
 		}
+	*/
 
 	return(0);
 }
